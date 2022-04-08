6508319db2fb5ae2dfbf09216f97a56dcdb8346e
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-04-08 18:04:37 -0500

    Con la gente de rAs

---
+++ app.py
@@ -0,0 +1,64 @@
+"""
+- Tipos de datos(LISTO)
+- Entradas y salidas
+- Funciones y Métodos
+- Parámetros y Argumentos
+"""
+
+""" TIPOS DE DATOS PRIMITIVOS """
+
+# Tipo de dato: STRING
+mochila="toalla"
+cartuchera="borrador"
+
+# FUNCIÓN PRINT()
+# print(cartuchera)
+
+# Tipo de dato: INT (enteros)
+edadJhomar=22
+edadFreddy=38
+
+# print(edadJhomar)
+
+# Tipo de dato: FLOAT (flotante)
+pi=3.1415
+alturaJhomar=1.79
+
+# Tipo de dato: BOOLEANO (bool)
+soleado=False
+lluvia=True 
+var_1="asdasdasd" #True
+var_2="" #False
+
+# print(var_1)
+
+"""TIPO DE DATOS COMPUESTOS"""
+# Listas, Diccionarios, Sets, Tuplas*****PENDIENTE*****
+
+""" Entradas y salidas """
+
+# Función de Salida: PRINT()
+print("Hola mundito, ya es viernes y tu cuerpo lo sabe!°!")
+
+# Función de Entrada: INPUT()
+nombre = input("Dime tu nombre: ") #Jhomar
+
+print(nombre)
+
+""" Funciones y Métodos """
+
+# import time 
+# time.sleep() #Método
+
+# Función
+def suma(num1, num2):
+    sumaAns=num1+num2
+    print(sumaAns)
+
+num1=5
+num2=3
+# suma1_2=num1+num2
+suma(num1 , num2)
+
+""" Parámetros y Argumentos """
+
---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hola mundo")
+
+function suma(nombre, edad){
+    console.log("Tu nombre es: "+nombre+"Tu edad es: "+edad)
+    return 1;
+}
+
+suma("jhomar", 22);
+
+// Tu nombre es: Jhomar Tu edad es: 22
